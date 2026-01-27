1a3d5f41e62ebeb11ef426dfbed9d9f097340a34
DJ Sproul <djminor@byu.edu>
2026-01-27 16:22:23 -0700

    Modified eslint config file to be cleaner

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.test.js"], languageOptions: { globals: { ...globals.node, ...globals.jest } } }
+  {languageOptions: { globals: { ...globals.node, ...globals.jest } }}
 ]);
