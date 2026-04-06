56393e46e75af2f9ea2d8725eb088cf5c0f4c97f
DJ Sproul <djminor@byu.edu>
2026-04-06 00:07:17 -0600

    Should be chaos testing ready!

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -45,6 +45,28 @@ orderRouter.docs = [
   },
 ];
 
+// Chaos endpoint (admin only)
+let enableChaos = false;
+orderRouter.put(
+  '/chaos/:state',
+  authRouter.authenticateToken,
+  asyncHandler(async (req, res) => {
+    if (req.user.isRole(Role.Admin)) {
+      enableChaos = req.params.state === 'true';
+    }
+
+    res.json({ chaos: enableChaos });
+  })
+);
+
+orderRouter.post('/', asyncHandler(async (req, res, next) => {
+  if (enableChaos && Math.random() < 0.5) {
+    metrics.trackPizzaCreationFailure();
+    throw new StatusCodeError('Chaos monkey', 500);
+  }
+  next();
+}));
+
 // getMenu
 orderRouter.get(
   '/menu',
