2c1606828e984f9d6156591367c1dd5ecc47a447
DJ Sproul <djminor@byu.edu>
2026-04-09 16:35:11 -0600

    Shored up update user vulnerability

--- src/database/database.js
+++ src/database/database.js
@@ -107,21 +107,25 @@ class DB {
     const connection = await this.getConnection();
     try {
       const params = [];
+      const values = [];
       if (password) {
         const hashedPassword = await bcrypt.hash(password, 10);
-        params.push(`password='${hashedPassword}'`);
+        params.push('password=?');
+        values.push(hashedPassword);
       }
       if (email) {
-        params.push(`email='${email}'`);
+        params.push('email=?');
+        values.push(email);
       }
       if (name) {
-        params.push(`name='${name}'`);
+        params.push('name=?');
+        values.push(name);
       }
       if (params.length > 0) {
-        const query = `UPDATE user SET ${params.join(', ')} WHERE id=${userId}`;
-        await this.query(connection, query);
+        values.push(userId);
+        await this.query(connection, `UPDATE user SET ${params.join(', ')} WHERE id=?`, values);
       }
-      return this.getUser(email, password);
+      return this.getUser(email, null);
     } finally {
       connection.end();
     }
